308d30cecc622722db5c2dd4de8360be04af77e0
Ricardo Magalhães <ricardo_magalhaes45@hotmail.com>
2017-12-05 11:05:37 +0000

    Arrow line + dynamic thickness

--- ARNaturalMarkers.py
+++ ARNaturalMarkers.py
@@ -8,6 +8,7 @@ img = cv2.imread("images/estadio.jpg")
 drawing = False  # true if mouse is pressed
 mode = True  # if True, draw normal line. Press 'm' to arrowed line
 ix, iy = -1, -1
+thickness = 5
 
 
 # mouse callback function
@@ -21,18 +22,18 @@ def draw_circle(event, x, y, flags, param):
 
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing is True:
+            img = imgcopy.copy()
             if mode is True:
-                img = imgcopy.copy()
-                cv2.line(img, (ix, iy), (x, y), (0, 0, 255), 15)
+                cv2.line(img, (ix, iy), (x, y), (0, 0, 255), thickness)
             else:
-                cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+                cv2.arrowedLine(img, (ix, iy), (x, y), (0, 0, 255), thickness)
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         if mode is True:
-            cv2.line(img, (ix, iy), (x, y), (0, 255, 0), 15)
+            cv2.line(img, (ix, iy), (x, y), (0, 0, 255), thickness)
         else:
-            cv2.circle(img, (x, y), 5, (0, 0, 255), -1)
+            cv2.arrowedLine(img, (ix, iy), (x, y), (0, 0, 255), thickness)
 
 
 cv2.namedWindow('image')
@@ -43,5 +44,9 @@ while(1):
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         mode = not mode
+    elif k == ord('2'):
+        thickness += 1
+    elif k == ord('1'):
+            thickness -= 1
     elif k == 27:
         break
